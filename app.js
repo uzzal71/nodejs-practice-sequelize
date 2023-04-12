bad2073ef8396b5ed79117d26c2485651ef2cbe7
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-13 00:26:31 +0600

    App export

--- app.js
+++ app.js
@@ -6,15 +6,4 @@ const app = express();
 // Use packages
 app.use(bodyParser.json());
 
-// Routes
-app.get("/", (req, res) => {
-    res.status(200).json({
-        "status": 200,
-        "message": "Nodejs sequelize!"
-    });
-});
-
-// Server
-app.listen(3000, (req, res) => {
-    console.log(`Server is running on http://localhost:${3000}`);
-});
+export default app;
