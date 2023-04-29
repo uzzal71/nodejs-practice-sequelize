d7aab05c62d70631d3268b7e36209e541bacba8d
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-30 01:12:44 +0600

    Team model define

--- models/Team.js
+++ models/Team.js
@@ -1,2 +1,11 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/sequelize";
+
+const Team = sequelize.define('Team', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+export default Team;
