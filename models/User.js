023ead1787eb843c1d8a0e7b12078ab485bc7962
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-29 22:52:12 +0600

    Address model import DataTypes & sequelize

--- models/User.js
+++ models/User.js
@@ -1,1 +1,21 @@
-import
+import { DataTypes } from "sequelize";
+import sequelize from '../config/sequelize';
+
+const User = sequelize.define('User', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }
+});
+
+export default User;
