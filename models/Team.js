1bb3d2ca8079932a88e335a2be4d84429d6f6ad9
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-30 01:26:01 +0600

    Team has many define

--- models/Team.js
+++ models/Team.js
@@ -1,11 +1,17 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/sequelize";
+import Player from "./Player";
 
 const Team = sequelize.define('Team', {
     name: {
         type: DataTypes.STRING,
         allowNull: false
     }
+});
+
+Team.hasMany(Player, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
 });
 
 export default Team;
