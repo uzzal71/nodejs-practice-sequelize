194eee275e0ca36bc387dd5bacf426fcdec92d5e
uzzal71 <uzzalroy.acm@gmail.com>
2023-04-30 01:22:07 +0600

    Player model define team id

--- models/Player.js
+++ models/Player.js
@@ -1,6 +1,16 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/sequelize";
+import Team from "./Team";
 
-const Player = sequelize.define('Player', {});
+const Player = sequelize.define('Player', {
+    team_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Team,
+            key: 'id'
+        },
+        allowNull: false
+    }
+});
 
 export default Player;
