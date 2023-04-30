56635d0470c3123175d5b37c2aba264d5ce62970
uzzal71 <uzzalroy.acm@gmail.com>
2023-05-01 00:38:13 +0600

    Actor & Model Update

--- models/Actor.js
+++ models/Actor.js
@@ -1,6 +1,14 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/sequelize";
 
-const Actor = sequelize.define('Actor', {});
+const Actor = sequelize.define('Actor', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'actors',
+    timestamps: true
+});
 
 export default Actor;
--- models/Movie .js
+++ models/Movie .js
@@ -1,6 +1,14 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/sequelize";
 
-const Movie = sequelize.define('Movie', {});
+const Movie = sequelize.define('Movie', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'movies',
+    timestamps: true
+});
 
 export default Movie;
