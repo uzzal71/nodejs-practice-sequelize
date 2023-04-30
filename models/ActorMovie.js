030527d5ad4a980284fd4e4ce7118aa340ce097f
uzzal71 <uzzalroy.acm@gmail.com>
2023-05-01 00:43:26 +0600

    Actor Movie Model Update

--- models/ActorMovie.js
+++ models/ActorMovie.js
@@ -1,6 +1,28 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/sequelize";
+import Movie from "./Movie ";
+import Actor from "./Actor";
 
-const ActorMovie = sequelize.define('ActorMovie', {});
+const ActorMovie = sequelize.define('ActorMovie', {
+    movie_id: {
+        type: DataTypes.NUMBER,
+        references: {
+            model: Movie,
+            key: 'id'
+        },
+        allowNull: false
+    },
+    actor_id: {
+        type: DataTypes.STRING,
+        references: {
+            model: Actor,
+            key: 'id'
+        },
+        allowNull: false
+    }
+},{
+    tableName: 'actor_movies',
+    timestamps: true
+});
 
 export default ActorMovie;
