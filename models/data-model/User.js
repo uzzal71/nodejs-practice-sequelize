1e4c4789c476d85d743cdbfa4bc48c5d64444f55
uzzal71 <uzzalroy.acm@gmail.com>
2023-05-12 18:16:10 +0600

    create data-model folder

--- models/data-model/User.js
+++ models/data-model/User.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import sequelize from '../config/sequelize';
+import sequelize from '../../config/sequelize';
 import Address from "./Address";
 
 const User = sequelize.define('User', {
