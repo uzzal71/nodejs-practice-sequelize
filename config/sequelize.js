818cc086a5a05996d0ab043800e65900e0a0291c
Uzzal Roy <32016943+uzzal71@users.noreply.github.com>
2023-06-14 22:27:39 +0600

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -4,6 +4,7 @@ import config from ".";
 const sequelize = new Sequelize(config.DB_DATABASE, config.DB_USERNAME, config.DB_PASSWORD, {
     host: config.DB_HOST,
     dialect: config.DB_DIALECT
+    // database console disabled 
 });
 
 export default sequelize;
